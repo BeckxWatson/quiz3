2b8845f1ca7f63ec7a21cf52b67ef3e1324277e8
Kittichai <thammathip.p17@gmail.com>
2023-04-24 10:40:42 +0700

    bhaha

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class Program {
+    static void Main (string[] args) {
+        int numberitem = int.Parse(Console.ReadLine());
+        int[] num = new int[numberitem];
+        for(int i = 0; i < numberitem; i++) {
+            num [i] = int.Parse(Console.ReadLine());
+        }
+        int nameitem = int.Parse(Console.ReadLine());
+        int typeitem = int.Parse(Console.ReadLine());
+    }
+}
